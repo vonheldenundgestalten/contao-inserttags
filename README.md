a3bb43f0761090cd729c04ce7c9b154f71a76a01
Markus Milkereit <mm@vonheldenundgestalten.de>
2022-01-15 14:47:55 +0100

    bugfix on backend rendering

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # contao-inserttags
-New, clean insert tag handling, adding some custom logic to existing insert tags in Contao 4.
+New, clean insert tag handling, adding some custom logic to existing insert tags in Contao 4.x
+
+Also activates rendering of insert tags in the backend - especially nice for custom elements, included subtemplates, icons and many more tricks in content element organisation.
+
+Does not replace it on any kind of backend action that depends on insert tags, and we're not touching the HTML element - that usually is modules or other "bigger" includes.
 
 ###New tags
 
--- src/EventListener/ParseBackendTemplateListener.php
+++ src/EventListener/ParseBackendTemplateListener.php
@@ -14,12 +14,15 @@ class ParseBackendTemplateListener
     public function __invoke(string $buffer, string $template): string
     {
         if ('be_main' === $template) {
-            // modify only if we're not in any action mode
+            // modify only if we're not in any action mode, popup or dynamic context
             // modify only if there is no HTML content element involved
-            if(!Input::get('act')
+            if(
+                !Input::get('act')
+                AND !Input::get('context')
+                AND !Input::get('picker')
                 AND strpos($buffer, '<div class="cte_type published">HTML</div>') === false
                 AND strpos($buffer, '<div class="cte_type unpublished">HTML</div>') === false
-            ){                
+            ){
                 $objIt = new InsertTags();
                 $buffer = $objIt->replace($buffer, true);                
             }            
