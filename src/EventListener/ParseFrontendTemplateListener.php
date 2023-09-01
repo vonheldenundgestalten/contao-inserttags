3affca5a20a1719e77ea1ac338479196763dc2c5
Markus Milkereit <markus.milkereit@magmell.de>
2023-09-01 10:27:19 +0200

    error handling imprioved for PHP 8.x compatability

--- src/EventListener/ParseFrontendTemplateListener.php
+++ src/EventListener/ParseFrontendTemplateListener.php
@@ -18,10 +18,13 @@ class ParseFrontendTemplateListener
     public function __invoke(string $buffer, string $templateName, FrontendTemplate $template): string
     {
         if (TL_MODE == 'BE' && $templateName != 'ce_html') {
-            if (!$GLOBALS['objPage']) {
+            if (!isset($GLOBALS['objPage']) || !$GLOBALS['objPage']) {
                 $a = ArticleModel::findById(Input::get("id"));
                 $b = PageModel::findByPk($a->pid);
-                $GLOBALS['objPage'] = PageModel::findByPk($b->trail[0]);
+                
+                if($b->trail) {
+                    $GLOBALS['objPage'] = PageModel::findByPk($b->trail[0]);
+                }
             }
             $objIt = new InsertTags();
             $buffer = $objIt->replace($buffer, true);
